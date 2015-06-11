92c80ab24de3aee166c9f7e09af6a6caad79a6e2
Hank Schultz <hankschultz@gmail.com>
2015-06-10 22:33:20 -0400

    Created initial Raytrace shell with a functioning "Open" dialog.

---
+++ src/sample/Controller.java
@@ -0,0 +1,71 @@
+package sample;
+
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+
+public class Controller {
+    @FXML private TextField txtWidth;
+    @FXML private TextField txtHeight;
+    @FXML private TextField txtAntialiasing;
+
+    @FXML private Button btnOpen;
+    @FXML private Button btnRender;
+
+    @FXML private Canvas cnvsRender;
+
+    @FXML private BorderPane topPane;
+
+    public void init()
+    {
+        txtWidth.textProperty().addListener((observable, oldValue, newValue) -> {
+            OnWidthChanged(observable, oldValue, newValue);
+        });
+
+        txtHeight.textProperty().addListener((observable, oldValue, newValue) -> {
+            OnHeightChanged(observable, oldValue, newValue);
+        });
+
+        txtAntialiasing.textProperty().addListener((observable, oldValue, newValue) -> {
+            OnAntialiasingChanged(observable, oldValue, newValue);
+        });
+    }
+
+    private void OnAntialiasingChanged(ObservableValue<? extends String> observable, String oldValue, String newValue)
+    {
+    }
+
+    private void OnHeightChanged(ObservableValue<? extends String> observable, String oldValue, String newValue)
+    {
+    }
+
+
+    private void OnWidthChanged(ObservableValue<? extends String>  observable, String oldValue, String newValue)
+    {
+
+    }
+
+    @FXML private void OnWidthEnter(ActionEvent event)
+    {
+
+    }
+
+    @FXML private void OnOpen(ActionEvent event)
+    {
+        FileChooser chooser = new FileChooser();
+
+        chooser.setSelectedExtensionFilter(new FileChooser.ExtensionFilter("Raytrace Data File", ".dat"));
+        File selected = chooser.showOpenDialog(topPane.getScene().getWindow());
+    }
+
+    @FXML private void OnRender(ActionEvent event)
+    {
+    }
+}
